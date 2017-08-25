0488ecb73e20a835b9be6621e01088caa1877805
Michael Cornillon <contact@dwaps.fr>
2017-08-25 17:38:31 +0200

    Création de l'instance de Retrofit

--- app/src/main/java/fr/dwaps/testretrofit/MainActivity.java
+++ app/src/main/java/fr/dwaps/testretrofit/MainActivity.java
@@ -3,11 +3,21 @@ package fr.dwaps.testretrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://dwaps.fr/tests/bdd-to-json/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+        retrofit.create(PersonAPI.class);
     }
 }
